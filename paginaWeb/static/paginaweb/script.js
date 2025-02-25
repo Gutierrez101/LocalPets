b6a2dec5b9a8b574e44c87583344e0a6bf322d16
JDXIX <jdquimbita1@espe.edu.ec>
2025-02-24 22:05:12 -0500

    Add files via upload

---
+++ paginaWeb/static/paginaweb/script.js
@@ -0,0 +1,9 @@
+// script.js
+function ingresar() {
+    alert('Redirigiendo a la página de inicio de sesión...');
+}
+
+function registrar() {
+    alert('Redirigiendo a la página de registro de mascotas...');
+    window.location.href = '/registro_mascota_perdida/';
+}
---
+++ paginaWeb/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from .views import (
+    index, ingresarTabla1, ingresarTabla2, verTabla1s, verTabla1Unica,
+    obtener_detalles, actualizar_mascota, obtener_ubicacion, actualizar_ubicacion, login_view, new_user_view,
+    registro_mascota
+
+)
+#from .views import obtener_detalles,actualizar_mascota,obtener_ubicacion,actualizar_ubicacion
+urlpatterns = [
+    path("", index, name="index"),
+    path('login/', login_view, name='login'),
+    path("ingresarTabla1/", ingresarTabla1, name="ingresarTabla1"),
+    path("ingresarTabla2/", ingresarTabla2, name="ingresarTabla2"),
+    path("verTabla1s/", verTabla1s, name="verTabla1s"),
+    path("verTabla1Unica/<int:id>/", verTabla1Unica, name="verTabla1Unica"),
+    path('new_user/', new_user_view, name='new_user'),
+    path('registro_mascota_perdida/', registro_mascota, name='registro_mascota_perdida'),
+    path('mascota/<int:pk>/',obtener_detalles,name='obtener_detalles'),
+    path('mascota/<int:pk>/actualizar/', actualizar_mascota, name='actualizar_mascota'),
+    path('mascota/<int:pk>/ubicacion/', obtener_ubicacion, name='obtener_ubicacion'),
+    path('mascota/<int:pk>/ubicacion/actualizar/', actualizar_ubicacion, name='actualizar_ubicacion'),
+    
+]
