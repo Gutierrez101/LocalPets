368776396a0061b25a3bd6703d2b6dd93366be62
JDXIX <jdquimbita1@espe.edu.ec>
2025-02-24 22:04:27 -0500

    Delete paginaWeb directory

--- paginaWeb/static/paginaweb/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function ingresar() {
-    alert('Redirigiendo a la página de inicio de sesión...');
-}
-
-function registrar() {
-    alert('Redirigiendo a la página de registro de mascotas...');
-    window.location.href = '/registro_mascota_perdida/';
-}
